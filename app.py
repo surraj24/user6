f0be959904925d5f7a10cf48356f71c1e59ddfe4
surraj24 <surajagrawal24@gmail.com>
2017-03-14 14:33:56 +0530

    second

--- app.py
+++ app.py
@@ -49,7 +49,11 @@ def makeWebhookResult(req):
             "source": "apiai-onlinestore-shipping"
         }
     if req.get("result").get("action") == "user.id2":
-        speech = "Right."
+        result = req.get("result")
+        parameters = result.get("parameters")
+        id1 = parameters.get("user-id")
+        cost = {'Suraj':'192.168.1.1', 'Shubham':'192.168.2.1', 'Raju':'192.168.3.1', 'Yash':'192.168.4.1', 'Ravi':'192.168.5.1'}
+        speech = id1 + " .IP address of your router is -" + cost[id1]
         print("Response:")
         print(speech)
         return {
