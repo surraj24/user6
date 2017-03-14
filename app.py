0d73faa3fde1fc72a1725282fc903ec68956ff19
surraj24 <surajagrawal24@gmail.com>
2017-03-14 23:28:17 +0530

    fourth

--- app.py
+++ app.py
@@ -65,11 +65,24 @@ def makeWebhookResult(req):
             "source": "apiai-onlinestore-shipping"
         }
     if req.get("result").get("action") == "user.id3":
-        result = req.get("result")
-        parameters = result.get("parameters")
-        id1 = parameters.get("user-id")
-        cost = {'Suraj':'192.168.1.1', 'Shubham':'192.168.2.1', 'Raju':'192.168.3.1', 'Yash':'192.168.4.1', 'Ravi':'192.168.5.1'}
-        speech = id1 + " .IP address of your router is -" + cost[id1]
+        db = MySQLdb.connect("localhost","user","","python" )
+        cursor = db.cursor()
+        # Prepare SQL query to INSERT a record into the database.
+        sql = """INSERT INTO text(id,
+                 title,text)
+                 VALUES ('3', 'hello', 'Magdg')"""
+        try:
+           # Execute the SQL command
+           cursor.execute(sql)
+           # Commit your changes in the database
+           db.commit()
+        except:
+           # Rollback in case there is any error
+           db.rollback()
+
+        # disconnect from server
+        db.close()
+        speech = "Hello"
         print("Response:")
         print(speech)
         return {
